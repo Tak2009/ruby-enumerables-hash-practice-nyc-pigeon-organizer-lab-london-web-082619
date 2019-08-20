28a4aa6cf4abca1e5ebd2dc928d9c1f0c57bf544
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-20 16:13:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,10 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
+binding.pry
     value.each do |new_value, names|
       names.each do |name|
-binding.pry
+
 
         if !new_hash[name]
           new_hash[name] = {}
